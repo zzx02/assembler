b5b3a3d50e872ba29204b246db62970ebe77fc2c
ZZ ZX <Appler@ZZdeMacBook-Air.local>
2017-04-07 15:31:35 +0800

    add_R_type

--- assem.cpp
+++ assem.cpp
@@ -9,10 +9,48 @@
 #define EMPTY_type 0x00
 #define J_type 0x01
 #define I_type 0x02
-#define L_type 0x03
+#define R_type 0x03
 
 using namespace std;
 
+ /**************************************************** 
+  The instruction strctures 
+  R-type:  
+     +------------------------------------------------+ 
+     | opcode | rs    | rt    | rd    | shamt | funct |     
+     |--------+-------+-------+-------+-------+-------| 
+     | 6-bit  | 5-bit | 5-bit | 5-bit | 5-bit | 6-bit | 
+     +------------------------------------------------+ 
+  I-type: 
+     +------------------------------------------------+ 
+     | opcode | rs    | rt    | immediate             | 
+     |--------+-------+-------+-----------------------| 
+     | 6-bit  | 5-bit | 5-bit | 16-bit                | 
+     +------------------------------------------------+ 
+  J-type: 
+     +------------------------------------------------+ 
+     | opcode | jump address                          | 
+     |--------+---------------------------------------| 
+     | 6-bit  | 26-bit                                | 
+     +------------------------------------------------+ 
+ */  
+
+string Handle_Instruction_R(string instruction)
+{
+	string code;
+	string opt, rs, rt, rd;
+	stringstream is(instruction);
+	is >> opt >> rd >> rs >> rt;
+	rs = rs.substr(0, rs.size() - 1);
+	rt = rs.substr(0, rt.size() - 1);
+	rd = rd.substr(0, rd.size() - 1);
+	switch(opt)
+	{
+		case("add"): code = "000000" + reg_lookup(rs) + reg_lookup(rt) + reg_lookup(rd) + "00000" + "100000";break;
+		case("sub"): code = "000000" + reg_lookup(rs) + reg_lookup(rt) + reg_lookup(rd) + "00000" + "100010";break;
+		case("and"): code = "000000" + reg_lookup(rs) + reg_lookup(rt) + reg_lookup(rd) + "00000" + "100100";break;
+	}	
+}
 int getmacro(string &s)
 {
   if (s.find('#') != string::npos)
@@ -48,7 +86,7 @@ int gettype(const string &s)
   }
   if (dollar == 0) return J_type;
   else if (dollar == 2) return I_type;
-  else return L_type;
+  else return R_type;
 }
 
 void String_space_filter(string &s)
@@ -85,6 +123,7 @@ int main(int argc, char** argv)
       cout << "Can't open " << argv[2] << ", Try again" << endl;
     return -1;
   }
+/* Scan the whole .asm file, generate the instructions and the label tables; */
   while (!inFile.eof())
   {
     string buffer, macro = "";
@@ -99,9 +138,21 @@ int main(int argc, char** argv)
     }
     if (getlabel(buffer, label)) Labels.insert(pair<string,int>(label, pc));
     if ((type = gettype(buffer)) == EMPTY_type) continue;
-    cout << pc << endl;
+	Instructions.push_back(buffer);
     pc += 4;
-    cout << "Okay" << endl;
   }
+  for (vector<string>::iterator iter = Instructions.begin(); iter != Instructions.end(); ++ iter)
+	{
+		Instruction_type type;
+		string Machine_code;
+		type = getbuffer(*iter);
+		switch(type)
+		{
+			case (J_type): Machine_code = Handle_Instruction_J(*iter); break;
+			case (I_type): Machine_code = Handle_Instruction_I(*iter); break;
+			case (R_type): Machine_code = Handle_Instruction_R(*iter); break;
+			default : break;
+		}
+	}
   cout << Labels["start"];
 }
